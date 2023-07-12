7efa02a2cda1d37a46c83dd82a0ee557afc66954
Archit Thakur <87373890+architthakur2000@users.noreply.github.com>
2023-07-11 23:45:04 -0500

    Create send.php
    send requests

---
+++ send.php
@@ -0,0 +1,37 @@
+<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve form data
+  $name = $_POST["name"];
+  $subject = $_POST["subject"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // Validate input
+  if (empty($name) || empty($email) || empty($message)) {
+    // Handle empty fields
+    echo "Please fill in all required fields.";
+    exit;
+  }
+
+  // Set up email headers
+  $to = "architthakur9@outlook.com"; 
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Construct email content
+  $emailContent = "Name: $name\n";
+  $emailContent .= "Subject: $subject\n";
+  $emailContent .= "Email: $email\n\n";
+  $emailContent .= "Message:\n$message";
+
+  // Send the email
+  if (mail($to, $subject, $emailContent, $headers)) {
+    // Email sent successfully
+    echo "Thank you for your message. We'll be in touch soon!";
+  } else {
+    // Failed to send email
+    echo "Oops! Something went wrong. Please try again later.";
+  }
+}
+?>
